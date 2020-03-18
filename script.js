4fcb1650d0f8756032786fffef0d13d4b3e215d4
Chris W <wells457@umn.edu>
2020-03-17 19:55:24 -0500

    More comments added to script'

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
+// This makes sure all of the HTML runs before the javascript
 $(document).ready(function() {
+  // This creates an event listener for the search button. When it is clicked, the following call-back fucntion happens
   $("#search-button").on("click", function() {
+    // this variable obtains the value in the search-value ID
     var searchValue = $("#search-value").val();
 
     // clear input box
+    // This makes it so when values are rendered, they don't get rendered twice
     $("#search-value").val("");
-
+    // This calls the searchWeather function, passing in the searchValue parameter 
     searchWeather(searchValue);
   });
-
+// This creates an event listener for when you click on a previously searched city that is seen below thanks to local storage
   $(".history").on("click", "li", function() {
+    // It calls the searchWeather function, passing in the name of the city
     searchWeather($(this).text());
   });
-
+  // this makeRow function dynamically creates a list item that is appended to the unordered list with the history class
+  // A "text" argument must be passed into this function, and it is this "text" which will be added to the list
   function makeRow(text) {
     var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
     $(".history").append(li);
   }
-
+  // This is the searchWeather function. It needs a "searchValue" arguemnet passed in
   function searchWeather(searchValue) {
+    // It makes an ajax call to an api
     $.ajax({
+      // It will be getting info from the api
       type: "GET",
+      // this is the url used to connect to the api. The search will use whatever searchValue was passed into it
       url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=600327cb1a9160fea2ab005509d1dc6d&units=imperial",
+      // The data will be returned in a json format
       dataType: "json",
+      // If successful, this function will run using the returned dara 
+      // TODO look up how this is different from then
       success: function(data) {
         // create history link for this search
         if (history.indexOf(searchValue) === -1) {
+          // add the search value to the history array
+          // TODO look up the history array
           history.push(searchValue);
+          // save a string version of the array to the local storage
           window.localStorage.setItem("history", JSON.stringify(history));
-    
+          // Call the makeRow function passing in the searchValue
           makeRow(searchValue);
         }
         
         // clear any old content
+        // We do this for the "today" div, so there's only one date showing at a time
         $("#today").empty();
 
         // create html content for current weather
+        // this creates an h3 header with the card-title class and the text of the data, and the date
         var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+        
         var card = $("<div>").addClass("card");
         var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
         var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
